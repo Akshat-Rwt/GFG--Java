f867a87abc81a61327d7df7e5d62bed004c46e59
Akshat Rawat <akshatrawat84@gmail.com>
2024-03-25 22:22:02 +0530

    Swap Two Number

---
+++ Swap_Two_Number.java
@@ -0,0 +1,15 @@
+public class Swap_Two_Number {
+
+    public static void main( String [] arga ){
+    int a = 10 ;
+    int b = 20 ;
+    int temp = 0;
+
+    temp = a ;
+    a = b ;
+    b = temp ;
+
+    System.out.println(a);
+    System.out.println(b);
+    }
+}
